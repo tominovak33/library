e291b7a7249f07cdc333c622c50b70f554fb94b7
tominovak33 <tominovak33@gmail.com>
2015-07-28 16:58:59 +0100

    check content of PR file

---
+++ webhooks/github_pull_request/check_pr.sh
@@ -0,0 +1,16 @@
+FS=$'\n'; # Lets the for loop split the input on newline char
+counter=0;
+
+for line in $(cat pr_test.txt);
+  do
+  if [ $counter -eq 0 ];
+    then
+     pr_number="$line"
+  elif [ $counter -eq 1];
+    then
+    branch_name="$line"
+  fi
+  counter=$(expr $counter + 1)
+done
+
+./deploy_pr.sh "$pr_number" "$branch_name"
