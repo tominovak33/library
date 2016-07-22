2828862370a3d2412d4f488e7ae46115a37deb30
Tomi <tominovak33@users.noreply.github.com>
2016-07-22 09:15:45 +0100

    Create links_in_new_tab.js

---
+++ code-snippets/javascript/links_in_new_tab.js
@@ -0,0 +1,16 @@
+// Make all EXTERNAL links on a page open in a new tab.
+
+// How to use: 
+// 1: (Optionally) Minify this file.
+// 2: Include it in the bottom of your desired page(s) as a JS script. 
+
+var links = document.querySelectorAll('a[href]');
+
+[].forEach.call(links, function(link) {
+	url_value = link.href
+
+ if (url_value.indexOf(window.location.href) == -1) {
+	link.setAttribute("target","_blank")
+ } 
+ 
+});
